ab3507d21da7c5123d5b1a725e5a9b2b646f2403
Nathan Sadler <nssadler@my.waketech.edu>
2021-05-12 15:41:14 -0400

    Finish making forms functional

--- triangles/app.rb
+++ triangles/app.rb
@@ -1,11 +1,16 @@
 require('sinatra')
 require('sinatra/reloader')
+#require('triangle')
 also_reload('lib/**/*.rb')
 
 get ('/') do
   "This should be the page with the form"
+  erb(:triangle_form)
 end
 
-get('/triangle_:a_:b_:c') do
+get('/triangle') do
   "This should be the page with the result"
+  @triangle_type = Triangle.new(params[:side_a].to_f, params[:side_b].to_f,
+  params[:side_c].to_f).get_type
+  erb(:triangle_result)
 end
